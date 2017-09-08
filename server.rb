7ad840e591e7389c9d8800d8b6604fd539a1346e
Dmitriy Tonkushin <d.tonkushin@abbyy-ls.com>
2017-09-08 17:23:32 +0500

    Added page about school mediation and resolve problem with active links.

--- server.rb
+++ server.rb
@@ -4,3 +4,8 @@ require 'slim'
 get '/' do
   slim :home
 end
+
+get '/school-mediation' do
+  @files = Dir.entries("./public/files/school-mediation").select {|f| !File.directory? f}
+  slim :school_mediation
+end
