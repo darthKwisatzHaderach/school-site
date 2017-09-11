4384be52e81212f1421b2a85c0aea646c08a5bcb
Dmitriy Tonkushin <d.tonkushin@abbyy-ls.com>
2017-09-11 14:58:14 +0500

    Created page about material base development.

--- server.rb
+++ server.rb
@@ -31,3 +31,12 @@ get '/anti-corruption-activity' do
   @zip_files = Dir.chdir(basedir) { Dir.glob("*.*z*") }
   slim :anti_corruption_activity
 end
+
+get '/material-base-development' do
+  basedir = "./public/files/material-base-development"
+  @dir = "files/material-base-development"
+  @pdf_files = Dir.chdir(basedir) { Dir.glob("*.pdf") }
+  @doc_files = Dir.chdir(basedir) { Dir.glob("*.doc*") }
+  @zip_files = Dir.chdir(basedir) { Dir.glob("*.*z*") }
+  slim :material_base_development
+end
