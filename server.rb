b6fabd71f46ccdaf589581ad5164b624b92ac48d
Dmitriy Tonkushin <d.tonkushin@abbyy-ls.com>
2017-09-12 17:39:16 +0500

    Added pages about education and socialization.

--- server.rb
+++ server.rb
@@ -9,6 +9,7 @@ Encoding.default_internal = Encoding::UTF_8
 @@pdf_icon = "files/common/pdf_icon.png"
 @@doc_icon = "files/common/doc_icon.png"
 @@zip_icon = "files/common/zip_icon.png"
+@@html_icon = "files/common/html_icon.png"
 
 get '/' do
   slim :home
@@ -39,6 +40,34 @@ get '/material-base-development' do
   @doc_files = Dir.chdir(basedir) { Dir.glob("*.doc*") }
   @zip_files = Dir.chdir(basedir) { Dir.glob("*.*z*") }
   slim :material_base_development
+end
+
+get '/strategy-of-development-of-education' do
+  basedir = "./public/files/strategy-of-development-of-education"
+  @dir = "files/strategy-of-development-of-education"
+  @pdf_files = Dir.chdir(basedir) { Dir.glob("*.pdf") }
+  @doc_files = Dir.chdir(basedir) { Dir.glob("*.doc*") }
+  @zip_files = Dir.chdir(basedir) { Dir.glob("*.*z*") }
+  slim :strategy_of_development_of_education
+end
+
+get '/patriotic-education' do
+  basedir = "./public/files/patriotic-education"
+  @dir = "files/patriotic-education"
+  @pdf_files = Dir.chdir(basedir) { Dir.glob("*.pdf") }
+  @doc_files = Dir.chdir(basedir) { Dir.glob("*.doc*") }
+  @zip_files = Dir.chdir(basedir) { Dir.glob("*.*z*") }
+  slim :patriotic_education
+end
+
+get '/culture-of-healthy-lifestyle' do
+  basedir = "./public/files/culture-of-healthy-lifestyle"
+  @dir = "files/culture-of-healthy-lifestyle"
+  @pdf_files = Dir.chdir(basedir) { Dir.glob("*.pdf") }
+  @doc_files = Dir.chdir(basedir) { Dir.glob("*.doc*") }
+  @zip_files = Dir.chdir(basedir) { Dir.glob("*.*z*") }
+  @html_files = Dir.chdir(basedir) { Dir.glob("*.htm*") }
+  slim :culture_of_healthy_lifestyle
 end
 
 get '/labor-union' do
